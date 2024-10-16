e310fefc57552427461108c69cdc0ddd7baa32f5
unknown <nipcts3@gmail.com>
2024-10-16 20:38:21 +0200

    configured

--- course1/packages/user_repository/src/firebase_user_repo.dart
+++ course1/packages/user_repository/src/firebase_user_repo.dart
@@ -1,1 +1,40 @@
-class FirebaseUserRepo {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'user_repo.dart';
+
+class FirebaseUserRepo implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  final useCollections = FirebaseFirestore.instance.collection('users');
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setUserData(user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<dynamic> signUp(myUser, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement user
+  Stream<dynamic> get user => throw UnimplementedError();
+}
--- course1/packages/user_repository/src/user_repo.dart
+++ course1/packages/user_repository/src/user_repo.dart
@@ -1,3 +1,8 @@
 abstract class UserRepository {
-  St
+  Stream<MyUser?> get user;
+  Future<MyUser> signUp(MyUser myUser, String password);
+  Future<void> setUserData(MyUser user);
+  Future<void> signIn(String email, String password);
+  Future<void> logOut();
 }
+ 
