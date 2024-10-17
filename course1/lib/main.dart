7e7837ab7105879bed8398847b03e5e623aa4ee3
unknown <nipcts3@gmail.com>
2024-10-17 20:54:44 +0200

    configured the main app

--- course1/lib/main.dart
+++ course1/lib/main.dart
@@ -1,15 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:course1/app.dart';
+import 'package:course1/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-    runApp(const MainApp());
-}
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-        ("Hello world"),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const MyApp());
 }
