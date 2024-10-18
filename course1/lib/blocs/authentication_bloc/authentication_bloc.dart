2100b483778d9cffe66439517bb93934dada82c5
unknown <nipcts3@gmail.com>
2024-10-19 01:08:38 +0200

    auth

--- course1/lib/blocs/authentication_bloc/authentication_bloc.dart
+++ course1/lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -5,8 +5,12 @@ import 'package:user_repository/user_repository.dart';
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
-class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationInitial()) {
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+  late final StreamSubscription<MyUser> _userSubscription;
+  AuthenticationBloc({required this._userSubscription})
+      : super(AuthenticationInitial()) {
     on<AuthenticationEvent>((event, emit) {
       // TODO: implement event handler
     });
--- course1/lib/blocs/authentication_bloc/authentication_state.dart
+++ course1/lib/blocs/authentication_bloc/authentication_state.dart
@@ -1,10 +1,23 @@
 part of 'authentication_bloc.dart';
 
-sealed class AuthenticationState extends Equatable {
-  const AuthenticationState();
-  
-  @override
-  List<Object> get props => [];
-}
 
-final class AuthenticationInitial extends AuthenticationState {}
+enum AuthenticationStatus {authenticated, unauthenticated, unknown}
+class AuthenticationState extends Equatable{
+  const AuthenticationState._({
+    this.status = AuthenticationStatus.unknown,
+    this.user
+
+  });
+  final AuthenticationStatus status;
+  final MyUser? user;
+
+const AuthenticationState.unknown() : this._();
+const AuthenticationState.authenticated(MyUser myUser) :
+this._(status: AuthenticationStatus.authenticated, user: myUser);
+
+const AuthenticationState.unauthenticated():
+this._(status: AuthenticationStatus.unauthenticated, user: null);
+
+  @override
+  List<Object?> get props => [status, user];
+}
