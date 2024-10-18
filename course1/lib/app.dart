e942c54945f13f9d4d4bdae1cc0efbf491e59b1f
unknown <nipcts3@gmail.com>
2024-10-18 12:06:59 +0200

    bloc

--- course1/lib/app.dart
+++ course1/lib/app.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import 'packages:'
+import 'package:user_repository/user_repository.dart';
+
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
-  const MyApp({super.key});
+
+  // Correctly assigning the parameter to the class property
+  const MyApp({super.key, required this.userRepository});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Your App Title',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: const Placeholder(), // You can replace this with your actual widget tree
+      ),
+    );
   }
 }
--- course1/lib/main.dart
+++ course1/lib/main.dart
@@ -3,10 +3,11 @@ import 'package:course1/app.dart';
 import 'package:course1/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MyApp());
+  runApp(MyApp(userRepository: FirebaseUserRepo(),)); 
 }
