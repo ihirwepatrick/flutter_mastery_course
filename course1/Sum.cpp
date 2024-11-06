a2002202c6b000d9e272c0e5b2e8d8223ef5252f
unknown <nipcts3@gmail.com>
2024-11-06 17:09:25 +0200

    firebase integration

---
+++ course1/Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() { 
+    int a, b, sum; 
+
+    cout << "Enter 2 numbers" << endl;
+    cin >> a >> b;
+    sum = a + b; 
+    cout << "Sum is " << sum << endl;
+
+    return 0; 
+}
